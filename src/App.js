67caec33d268c9067007fd12144d1c63996c96a8
ninaclb <ninacarolinabarater@gmail.com>
2023-03-27 14:37:28 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,83 +1,63 @@
 import './css/App.css';
 import { useEffect, useState } from 'react';
 
-const mock = [
-  {
-    "id": 1,
-    "name": "Rick Sanchez",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-    "name": "Earth (C-137)",
-    "url": "https://rickandmortyapi.com/api/location/1"
-    },
-    "location": {
-    "name": "Citadel of Ricks",
-    "url": "https://rickandmortyapi.com/api/location/3"
-    },
-    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    "episode": [
-    "https://rickandmortyapi.com/api/episode/1",
-    "https://rickandmortyapi.com/api/episode/2",
-    "https://rickandmortyapi.com/api/episode/3",
-    "https://rickandmortyapi.com/api/episode/4",
-    "https://rickandmortyapi.com/api/episode/5",
-    "https://rickandmortyapi.com/api/episode/6",
-    "https://rickandmortyapi.com/api/episode/7",
-    "https://rickandmortyapi.com/api/episode/8",
-    "https://rickandmortyapi.com/api/episode/9",
-    "https://rickandmortyapi.com/api/episode/10",
-    "https://rickandmortyapi.com/api/episode/11",
-    "https://rickandmortyapi.com/api/episode/12",
-    "https://rickandmortyapi.com/api/episode/13",
-    "https://rickandmortyapi.com/api/episode/14",
-    "https://rickandmortyapi.com/api/episode/15",
-    "https://rickandmortyapi.com/api/episode/16",
-    "https://rickandmortyapi.com/api/episode/17",
-    "https://rickandmortyapi.com/api/episode/18",
-    "https://rickandmortyapi.com/api/episode/19",
-    "https://rickandmortyapi.com/api/episode/20",
-    "https://rickandmortyapi.com/api/episode/21",
-    "https://rickandmortyapi.com/api/episode/22",
-    "https://rickandmortyapi.com/api/episode/23",
-    "https://rickandmortyapi.com/api/episode/24",
-    "https://rickandmortyapi.com/api/episode/25",
-    "https://rickandmortyapi.com/api/episode/26",
-    "https://rickandmortyapi.com/api/episode/27",
-    "https://rickandmortyapi.com/api/episode/28",
-    "https://rickandmortyapi.com/api/episode/29",
-    "https://rickandmortyapi.com/api/episode/30",
-    "https://rickandmortyapi.com/api/episode/31",
-    "https://rickandmortyapi.com/api/episode/32",
-    "https://rickandmortyapi.com/api/episode/33",
-    "https://rickandmortyapi.com/api/episode/34",
-    "https://rickandmortyapi.com/api/episode/35",
-    "https://rickandmortyapi.com/api/episode/36",
-    "https://rickandmortyapi.com/api/episode/37",
-    "https://rickandmortyapi.com/api/episode/38",
-    "https://rickandmortyapi.com/api/episode/39",
-    "https://rickandmortyapi.com/api/episode/40",
-    "https://rickandmortyapi.com/api/episode/41",
-    "https://rickandmortyapi.com/api/episode/42",
-    "https://rickandmortyapi.com/api/episode/43",
-    "https://rickandmortyapi.com/api/episode/44",
-    "https://rickandmortyapi.com/api/episode/45",
-    "https://rickandmortyapi.com/api/episode/46",
-    "https://rickandmortyapi.com/api/episode/47",
-    "https://rickandmortyapi.com/api/episode/48",
-    "https://rickandmortyapi.com/api/episode/49",
-    "https://rickandmortyapi.com/api/episode/50",
-    "https://rickandmortyapi.com/api/episode/51"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/1",
-    "created": "2017-11-04T18:48:46.250Z"
-    }
-]
-
 function App() {
   const [conteudo, setConteudo] = useState(<></>)
+
+  function traduzirStatus(status){
+    switch(status){
+      case 'Alive':
+        return 'Vivo'
+      case 'Dead':
+        return 'Morto'
+      case 'unknown':
+        return 'Desconhecido'
+      default:
+        return status
+    }
+  }
+  function traduzirEspecie(especie){
+    switch(especie){
+      case 'Human':
+        return 'Humano'
+      case 'Alien':
+        return 'Alien'
+      case 'Humanoid':
+        return 'Humanoide'
+      case 'Poopybutthole':
+        return 'Poopybutthole'
+      case 'Mythological Creature':
+        return 'Criatura Mítica'
+      case 'Animal':
+        return 'Animal'
+      case 'Robot':
+        return 'Robô'
+      case 'Disease':
+        return 'Doença'
+      case 'Cronenberg':
+        return 'Cronenberg'
+      case 'unknown':
+        return 'Desconhecido'
+      case 'Planet':
+        return 'Planeta'
+      default:
+        return especie
+    }
+  }
+  function traduzirGenero(genero){
+    switch(genero){
+      case 'Male':
+        return 'Masculino'
+      case 'Female':
+        return 'Feminino'
+      case 'Genderless':
+        return 'Sem Gênero'
+      case 'unknown':
+        return 'Desconhecido'
+      default:
+        return genero
+    }}
+
 
   async function carregarTodosOsPersonagens(){
     const retorno = await fetch(
@@ -87,7 +67,6 @@ function App() {
     .then(resposta => resposta.json())
     return retorno.results
   }
-
   async function listaPersonagem(){
     const todosPersonagens = await carregarTodosOsPersonagens()
 
@@ -98,24 +77,26 @@ function App() {
         <div className='char-info'>
           <span>
           <b>Espécie: </b>
-          {personagem.species}
+          {traduzirEspecie(personagem.species)}
           </span>
           <span>
-            <b>Gênero: </b>
-            {personagem.gender}
+          <b>Gênero: </b>
+            {traduzirGenero(personagem.gender)}
           </span>
         </div>
         <div>
         <div className='lista-secundaria '>
-          <p>Participações: </p>
+          <b>Participações: </b>
           {personagem.episode.map(ep => (
-          <></>
+            <span key={personagem.name+(ep.split('episode/')[1])}>
+              Ep-{ep.split('episode/')[1]}
+            </span>
           ))}
         </div>
         </div>
 
         <h5>
-        <b>status: </b>{personagem.status}
+        <b>Status: </b>{traduzirStatus(personagem.status)}
         </h5>
         </div>
       )
@@ -133,6 +114,36 @@ function App() {
       <header className="cabecalho">
         <h1>Rick and Morty API</h1>
       </header>
+      <div className='filtros'>
+        <span className='filtros-titulo'>Filtros</span>
+        <div className='filtro'>
+          <b>Status:</b>
+          <span>Vivo</span>
+          <span>Morto</span>
+          <span>Desconhecido</span>
+        </div>
+        <div className='filtro'>
+          <b>Genero:</b>
+          <span>Masculino</span>
+          <span>Feminino</span>
+          <span>Sem Gênero</span>
+          <span>Desconhecido</span>
+        </div>
+        <div className='filtro'>
+          <b>Espécie:</b>
+          <span>Humano</span>
+          <span>Alien</span>
+          <span>Humanoide</span>
+          <span>Poopybutthole</span>
+          <span>Criatura Mítica</span>
+          <span>Animal</span>
+          <span>Robô</span>
+          <span>Doença</span>
+          <span>Cronenberg</span>
+          <span>Desconhecido</span>
+          <span>Planeta</span>
+        </div>
+      </div>
       <div className='lista-principal'>
         {conteudo}
       </div>
